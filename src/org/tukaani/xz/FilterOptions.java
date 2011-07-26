cf7407527b2d1bba5a4ab969ed9c7abc377bf5d2
Lasse Collin <lasse.collin@tukaani.org>
2011-07-26 11:38:55 +0300

    Add functions to calculate memory usage of a FilterOptions array.
    Using these functions may be a little more convenient than
    writing the required loop yourself.

--- src/org/tukaani/xz/FilterOptions.java
+++ src/org/tukaani/xz/FilterOptions.java
@@ -19,6 +19,36 @@ import java.io.IOException;
  */
 public abstract class FilterOptions implements Cloneable {
     /**
+     * Gets how much memory the encoder will need with
+     * the given filter chain. This function simply calls
+     * <code>getEncoderMemoryUsage()</code> for every filter
+     * in the array and returns the sum of the returned values.
+     */
+    public static int getEncoderMemoryUsage(FilterOptions[] options) {
+        int m = 0;
+
+        for (int i = 0; i < options.length; ++i)
+            m += options[i].getEncoderMemoryUsage();
+
+        return m;
+    }
+
+    /**
+     * Gets how much memory the decoder will need with
+     * the given filter chain. This function simply calls
+     * <code>getDecoderMemoryUsage()</code> for every filter
+     * in the array and returns the sum of the returned values.
+     */
+    public static int getDecoderMemoryUsage(FilterOptions[] options) {
+        int m = 0;
+
+        for (int i = 0; i < options.length; ++i)
+            m += options[i].getDecoderMemoryUsage();
+
+        return m;
+    }
+
+    /**
      * Gets how much memory the encoder will need with these options.
      */
     public abstract int getEncoderMemoryUsage();
