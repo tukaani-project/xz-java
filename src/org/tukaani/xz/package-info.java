1a5f92d5bcf994f10c746000551153d91596a460
Lasse Collin <lasse.collin@tukaani.org>
2011-08-17 21:11:40 +0300

    Update README and similar docs.

--- src/org/tukaani/xz/package-info.java
+++ src/org/tukaani/xz/package-info.java
@@ -4,14 +4,19 @@
  * <h4>Introduction</h4>
  * <p>
  * This aims to be a complete implementation of XZ data compression
- * in pure Java. Single-threaded streamed compression and decompression
- * have been fully implemented.
- * Planned features include threaded compression, threaded decompression,
- * and random access decompression. It is unknown when these will be
- * implemented.
+ * in pure Java. Features:
+ * <ul>
+ * <li>Full support for the .xz file format specification version 1.0.4</li>
+ * <li>Single-threaded streamed compression and decompression</li>
+ * <li>Single-threaded decompression with limited random access support</li>
+ * <li>Raw streams (no .xz headers) for advanced users, including LZMA2
+ *     with preset dictionary</li>
+ * </ul>
+ * <p>
+ * Threading is planned but it is unknown when it will be implemented.
  * <p>
  * For the latest source code, see the
- * <a href="http://tukaani.org/xz/java.html">home page of XZ in Java</a>.
+ * <a href="http://tukaani.org/xz/java.html">home page of XZ for Java</a>.
  *
  * <h4>Getting started</h4>
  * <p>
@@ -21,9 +26,11 @@
  * see also {@link org.tukaani.xz.SingleXZInputStream}.
  *
  * <h4>Licensing</h4>
- * This Java implementation of XZ has been put into the public domain,
- * thus you can do whatever you want with it. All the files in the package
- * have been written by Lasse Collin, but some files are heavily based on
- * public domain code written by Igor Pavlov.
+ * <p>
+ * XZ for Java has been put into the public domain, thus you can do
+ * whatever you want with it. All the files in the package have been
+ * written by Lasse Collin and/or Igor Pavlov.
+ * <p>
+ * This software is provided "as is", without any warranty.
  */
 package org.tukaani.xz;
