e5c91cd46c1aafab007181fa4a8af5ff772fc77e
Lasse Collin <lasse.collin@tukaani.org>
2011-08-19 09:57:26 +0300

    Improve javadocs in LZMA2Options.

--- src/org/tukaani/xz/LZMA2Options.java
+++ src/org/tukaani/xz/LZMA2Options.java
@@ -177,6 +177,16 @@ public class LZMA2Options extends FilterOptions {
 
     /**
      * Sets the compression options to the given preset.
+     * <p>
+     * The presets 0-3 are fast presets with medium compression.
+     * The presets 4-6 are fairly slow presets with high compression.
+     * The default preset (<code>PRESET_DEFAULT</code>) is 6.
+     * <p>
+     * The presets 7-9 are like the preset 6 but use bigger dictionaries
+     * and have higher compressor and decompressor memory requirements.
+     * Unless the uncompressed size of the file exceeds 8&nbsp;MiB,
+     * 16&nbsp;MiB, or 32&nbsp;MiB, it is waste of memory to use the
+     * presets 7, 8, or 9, respectively.
      *
      * @throws      UnsupportedOptionsException
      *                          <code>preset</code> is not supported
@@ -206,6 +216,12 @@ public class LZMA2Options extends FilterOptions {
 
     /**
      * Sets the dictionary size in bytes.
+     * <p>
+     * The dictionary (or history buffer) holds the most recently seen
+     * uncompressed data. Bigger dictionary usually means better compression.
+     * However, using a dictioanary bigger than the size of the uncompressed
+     * data is waste of memory.
+     * <p>
      * Any value in the range [DICT_SIZE_MIN, DICT_SIZE_MAX] is valid,
      * but sizes of 2^n and 2^n&nbsp;+&nbsp;2^(n-1) bytes are somewhat
      * recommended.
@@ -240,6 +256,13 @@ public class LZMA2Options extends FilterOptions {
      * <p>
      * <b>The .xz format doesn't support a preset dictionary for now.
      * Do not set a preset dictionary unless you use raw LZMA2.</b>
+     * <p>
+     * Preset dictionary can be useful when compressing many similar,
+     * relatively small chunks of data independently from each other.
+     * A preset dictionary should contain typical strings that occur in
+     * the files being compressed. The most probable strings should be
+     * near the end of the preset dictionary. The preset dictionary used
+     * for compression is also needed for decompression.
      */
     public void setPresetDict(byte[] presetDict) {
         this.presetDict = presetDict;
@@ -254,6 +277,10 @@ public class LZMA2Options extends FilterOptions {
 
     /**
      * Sets the number of literal context bits and literal position bits.
+     * <p>
+     * The sum of <code>lc</code> and <code>lp</code> is limited to 4.
+     * Trying to exceed it will throw an exception. This function lets
+     * you change both at the same time.
      *
      * @throws      UnsupportedOptionsException
      *                          <code>lc</code> and <code>lp</code>
@@ -272,6 +299,24 @@ public class LZMA2Options extends FilterOptions {
 
     /**
      * Sets the number of literal context bits.
+     * <p>
+     * All bytes that cannot be encoded as matches are encoded as literals.
+     * That is, literals are simply 8-bit bytes that are encoded one at
+     * a time.
+     * <p>
+     * The literal coding makes an assumption that the highest <code>lc</code>
+     * bits of the previous uncompressed byte correlate with the next byte.
+     * For example, in typical English text, an upper-case letter is often
+     * followed by a lower-case letter, and a lower-case letter is usually
+     * followed by another lower-case letter. In the US-ASCII character set,
+     * the highest three bits are 010 for upper-case letters and 011 for
+     * lower-case letters. When <code>lc</code> is at least 3, the literal
+     * coding can take advantage of this property in the  uncompressed data.
+     * <p>
+     * The default value (3) is usually good. If you want maximum compression,
+     * try <code>setLc(4)</code>. Sometimes it helps a little, and sometimes it
+     * makes compression worse. If it makes it worse, test for example
+     * <code>setLc(2)</code> too.
      *
      * @throws      UnsupportedOptionsException
      *                          <code>lc</code> is invalid, or the sum
@@ -284,6 +329,10 @@ public class LZMA2Options extends FilterOptions {
 
     /**
      * Sets the number of literal position bits.
+     * <p>
+     * This affets what kind of alignment in the uncompressed data is
+     * assumed when encoding literals. See {@link #setPb(int) setPb} for
+     * more information about alignment.
      *
      * @throws      UnsupportedOptionsException
      *                          <code>lp</code> is invalid, or the sum
@@ -310,6 +359,24 @@ public class LZMA2Options extends FilterOptions {
 
     /**
      * Sets the number of position bits.
+     * <p>
+     * This affects what kind of alignment in the uncompressed data is
+     * assumed in general. The default (2) means four-byte alignment
+     * (2^<code>pb</code> = 2^2 = 4), which is often a good choice when
+     * there's no better guess.
+     * <p>
+     * When the alignment is known, setting the number of position bits
+     * accordingly may reduce the file size a little. For example with text
+     * files having one-byte alignment (US-ASCII, ISO-8859-*, UTF-8), using
+     * <code>setPb(0)</code> can improve compression slightly. For UTF-16
+     * text, <code>setPb(1)</code> is a good choice. If the alignment is
+     * an odd number like 3 bytes, <code>setPb(0)</code> might be the best
+     * choice.
+     * <p>
+     * Even though the assumed alignment can be adjusted with
+     * <code>setPb</code> and <code>setLp</code>, LZMA2 still slightly favors
+     * 16-byte alignment. It might be worth taking into account when designing
+     * file formats that are likely to be often compressed with LZMA2.
      *
      * @throws      UnsupportedOptionsException
      *                          <code>pb</code> is invalid
@@ -331,6 +398,18 @@ public class LZMA2Options extends FilterOptions {
 
     /**
      * Sets the compression mode.
+     * <p>
+     * This specifies the method to analyze the data produced by
+     * a match finder. The default is <code>MODE_FAST</code> for presets
+     * 0-3 and <code>MODE_NORMAL</code> for presets 4-9.
+     * <p>
+     * Usually <code>MODE_FAST</code> is used with Hash Chain match finders
+     * and <code>MODE_NORMAL</code> with Binary Tree match finders. This is
+     * also what the presets do.
+     * <p>
+     * The special mode <code>MODE_UNCOMPRESSED</code> doesn't try to
+     * compress the data at all (and doesn't use a match finder) and will
+     * simply wrap it in uncompressed LZMA2 chunks.
      *
      * @throws      UnsupportedOptionsException
      *                          <code>mode</code> is not supported
@@ -352,9 +431,13 @@ public class LZMA2Options extends FilterOptions {
 
     /**
      * Sets the nice length of matches.
+     * Once a match of at least <code>niceLen</code> bytes is found,
+     * the algorithm stops looking for better matches. Higher values tend
+     * to give better compression at the expense of speed. The default
+     * depends on the preset.
      *
      * @throws      UnsupportedOptionsException
-     *                          <code>nice</code> is invalid
+     *                          <code>niceLen</code> is invalid
      */
     public void setNiceLen(int niceLen) throws UnsupportedOptionsException {
         if (niceLen < NICE_LEN_MIN)
@@ -379,6 +462,11 @@ public class LZMA2Options extends FilterOptions {
 
     /**
      * Sets the match finder type.
+     * <p>
+     * Match finder has a major effect on compression speed, memory usage,
+     * and compression ratio. Usually Hash Chain match finders are faster
+     * than Binary Tree match finders. The default depends on the preset:
+     * 0-3 use <code>MF_HC4</code> and 4-9 use <code>MF_BT4</code>.
      *
      * @throws      UnsupportedOptionsException
      *                          <code>mf</code> is not supported
@@ -401,9 +489,15 @@ public class LZMA2Options extends FilterOptions {
     /**
      * Sets the match finder search depth limit.
      * <p>
-     * The special value of <code>0</code> indicates that the depth limit
-     * should be automatically calculated by the selected match finder
-     * from the nice length of matches.
+     * The default is a special value of <code>0</code> which indicates that
+     * the depth limit should be automatically calculated by the selected
+     * match finder from the nice length of matches.
+     * <p>
+     * Reasonable depth limit for Hash Chain match finders is 4-100 and
+     * 16-1000 for Binary Tree match finders. Using very high values can
+     * make the compressor extremely slow with some files. Avoid settings
+     * higher than 1000 unless you are prepared to interrupt the compression
+     * in case it is taking far too long.
      *
      * @throws      UnsupportedOptionsException
      *                          <code>depthLimit</code> is invalid
