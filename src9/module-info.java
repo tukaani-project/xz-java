3dd2c2da01f363ceaa2bb16a2dd44f541f5dcd27
Lasse Collin <lasse.collin@tukaani.org>
2021-02-01 19:45:48 +0200

    Add module-info.java as multi-release JAR.
    Automatic-Module-Name was removed.

---
+++ src9/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * module-info
+ *
+ * Author: Lasse Collin <lasse.collin@tukaani.org>
+ *
+ * This file has been put into the public domain.
+ * You can do whatever you want with this file.
+ */
+
+module org.tukaani.xz {
+    exports org.tukaani.xz;
+}
