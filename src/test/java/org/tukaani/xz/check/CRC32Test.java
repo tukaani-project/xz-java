2798a10cc54767b5a96244a845c172f14636f6b7
Brett Okken <brett.okken.os@gmail.com>
2024-02-10 15:48:14 -0600

    use maven to build project
    additional unit test
    fix whitespaces in formatting of test classes

--- src/test/java/org/tukaani/xz/check/CRC32Test.java
+++ src/test/java/org/tukaani/xz/check/CRC32Test.java
@@ -19,50 +19,50 @@ public class CRC32Test {
     private static final byte[] EXPECTED = new byte[] { 115, -116, 5, 41 };
 
     static {
-	for (int i = 0; i < DATA.length; ++i) {
-	    DATA[i] = (byte) i;
-	}
+        for (int i = 0; i < DATA.length; ++i) {
+            DATA[i] = (byte) i;
+        }
     }
 
     @Test
     public void test() {
-	CRC32 check = new CRC32();
-	check.update(DATA);
-	byte[] actual = check.finish();
-	assertArrayEquals(EXPECTED, actual);
+        CRC32 check = new CRC32();
+        check.update(DATA);
+        byte[] actual = check.finish();
+        assertArrayEquals(EXPECTED, actual);
     }
 
     @Test
     public void test_1() {
-	CRC32 check = new CRC32();
-	for (int i = 0; i < DATA.length; ++i) {
-	    check.update(DATA, i, 1);
-	}
-	byte[] actual = check.finish();
-	assertArrayEquals(EXPECTED, actual);
+        CRC32 check = new CRC32();
+        for (int i = 0; i < DATA.length; ++i) {
+            check.update(DATA, i, 1);
+        }
+        byte[] actual = check.finish();
+        assertArrayEquals(EXPECTED, actual);
     }
 
     @Test
     public void test_7() {
-	CRC32 check = new CRC32();
-	int i = 0;
-	for (int j = DATA.length - 6; i < j; i += 7) {
-	    check.update(DATA, i, 7);
-	}
-	check.update(DATA, i, DATA.length - i);
-	byte[] actual = check.finish();
-	assertArrayEquals(EXPECTED, actual);
+        CRC32 check = new CRC32();
+        int i = 0;
+        for (int j = DATA.length - 6; i < j; i += 7) {
+            check.update(DATA, i, 7);
+        }
+        check.update(DATA, i, DATA.length - i);
+        byte[] actual = check.finish();
+        assertArrayEquals(EXPECTED, actual);
     }
 
     @Test
     public void test_17() {
-	CRC32 check = new CRC32();
-	int i = 0;
-	for (int j = DATA.length - 16; i < j; i += 17) {
-	    check.update(DATA, i, 17);
-	}
-	check.update(DATA, i, DATA.length - i);
-	byte[] actual = check.finish();
-	assertArrayEquals(EXPECTED, actual);
+        CRC32 check = new CRC32();
+        int i = 0;
+        for (int j = DATA.length - 16; i < j; i += 17) {
+            check.update(DATA, i, 17);
+        }
+        check.update(DATA, i, DATA.length - i);
+        byte[] actual = check.finish();
+        assertArrayEquals(EXPECTED, actual);
     }
 }
--- src/test/java/org/tukaani/xz/check/CRC64Test.java
+++ src/test/java/org/tukaani/xz/check/CRC64Test.java
@@ -19,50 +19,50 @@ public class CRC64Test {
     private static final byte[] EXPECTED = new byte[] { -80, 58, -37, 101, 47, 75, 65, 114 };
 
     static {
-	for (int i = 0; i < DATA.length; ++i) {
-	    DATA[i] = (byte) i;
-	}
+        for (int i = 0; i < DATA.length; ++i) {
+            DATA[i] = (byte) i;
+        }
     }
 
     @Test
     public void test() {
-	CRC64 check = new CRC64();
-	check.update(DATA);
-	byte[] actual = check.finish();
-	assertArrayEquals(EXPECTED, actual);
+        CRC64 check = new CRC64();
+        check.update(DATA);
+        byte[] actual = check.finish();
+        assertArrayEquals(EXPECTED, actual);
     }
 
     @Test
     public void test_1() {
-	CRC64 check = new CRC64();
-	for (int i = 0; i < DATA.length; ++i) {
-	    check.update(DATA, i, 1);
-	}
-	byte[] actual = check.finish();
-	assertArrayEquals(EXPECTED, actual);
+        CRC64 check = new CRC64();
+        for (int i = 0; i < DATA.length; ++i) {
+            check.update(DATA, i, 1);
+        }
+        byte[] actual = check.finish();
+        assertArrayEquals(EXPECTED, actual);
     }
 
     @Test
     public void test_7() {
-	CRC64 check = new CRC64();
-	int i = 0;
-	for (int j = DATA.length - 6; i < j; i += 7) {
-	    check.update(DATA, i, 7);
-	}
-	check.update(DATA, i, DATA.length - i);
-	byte[] actual = check.finish();
-	assertArrayEquals(EXPECTED, actual);
+        CRC64 check = new CRC64();
+        int i = 0;
+        for (int j = DATA.length - 6; i < j; i += 7) {
+            check.update(DATA, i, 7);
+        }
+        check.update(DATA, i, DATA.length - i);
+        byte[] actual = check.finish();
+        assertArrayEquals(EXPECTED, actual);
     }
 
     @Test
     public void test_17() {
-	CRC64 check = new CRC64();
-	int i = 0;
-	for (int j = DATA.length - 16; i < j; i += 17) {
-	    check.update(DATA, i, 17);
-	}
-	check.update(DATA, i, DATA.length - i);
-	byte[] actual = check.finish();
-	assertArrayEquals(EXPECTED, actual);
+        CRC64 check = new CRC64();
+        int i = 0;
+        for (int j = DATA.length - 16; i < j; i += 17) {
+            check.update(DATA, i, 17);
+        }
+        check.update(DATA, i, DATA.length - i);
+        byte[] actual = check.finish();
+        assertArrayEquals(EXPECTED, actual);
     }
 }
