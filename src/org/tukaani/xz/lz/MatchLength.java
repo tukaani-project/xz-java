a6947623e810664a599fa4a8a197f7f5e8888042
Lasse Collin <lasse.collin@tukaani.org>
2024-03-22 15:48:51 +0200

    Add minimal MatchLength with a static getLen method
    Eventually this will be used only with Java 8 (or older).

---
+++ src/org/tukaani/xz/lz/MatchLength.java
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: 0BSD
+// SPDX-FileCopyrightText: The XZ for Java authors and contributors
+// SPDX-FileContributor: Lasse Collin <lasse.collin@tukaani.org>
+
+package org.tukaani.xz.lz;
+
+final class MatchLength {
+    static int getLen(byte[] buf, int off, int delta, int len, int lenLimit) {
+        assert off >= 0;
+        assert delta > 0;
+        assert len >= 0;
+        assert lenLimit >= len;
+
+        lenLimit += off;
+        int i = off + len;
+
+        while (i < lenLimit && buf[i] == buf[i - delta])
+            ++i;
+
+        return i - off;
+    }
+}
