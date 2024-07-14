e47a2a2b5d268079e946e626bfc6e3f136cd4242
Lasse Collin <lasse.collin@tukaani.org>
2024-07-14 19:07:39 +0300

    Refactor the PowerPC filter

--- src/org/tukaani/xz/simple/PowerPC.java
+++ src/org/tukaani/xz/simple/PowerPC.java
@@ -5,6 +5,8 @@
 
 package org.tukaani.xz.simple;
 
+import org.tukaani.xz.common.ByteArrayView;
+
 // BCJ filter for big endian PowerPC instructions
 public final class PowerPC implements SimpleFilter {
     private final boolean isEncoder;
@@ -22,21 +24,14 @@ public final class PowerPC implements SimpleFilter {
 
         for (i = off; i <= end; i += 4) {
             if ((buf[i] & 0xFC) == 0x48 && (buf[i + 3] & 0x03) == 0x01) {
-                int src = ((buf[i] & 0x03) << 24)
-                          | ((buf[i + 1] & 0xFF) << 16)
-                          | ((buf[i + 2] & 0xFF) << 8)
-                          | (buf[i + 3] & 0xFC);
+                int instr = ByteArrayView.getIntBE(buf, i);
 
-                int dest;
-                if (isEncoder)
-                    dest = src + (pos + i - off);
-                else
-                    dest = src - (pos + i - off);
+                int pc = pos + i - off;
+                if (!isEncoder)
+                    pc = -pc;
 
-                buf[i] = (byte)(0x48 | ((dest >>> 24) & 0x03));
-                buf[i + 1] = (byte)(dest >>> 16);
-                buf[i + 2] = (byte)(dest >>> 8);
-                buf[i + 3] = (byte)((buf[i + 3] & 0x03) | dest);
+                instr = 0x48000001 | ((instr + pc) & 0x03FFFFFC);
+                ByteArrayView.setIntBE(buf, i, instr);
             }
         }
 
