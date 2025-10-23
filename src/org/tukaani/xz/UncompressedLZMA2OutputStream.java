tukaani-project/xz-java

commit daface1a5537e5ac37655ae0be696a13e8121f6c

Add LZMA2 encoder.

src/org/tukaani/xz/UncompressedLZMA2OutputStream.java
@@ -1,0 +1,84 @@
+/*
+ * UncompressedLZMA2OutputStream
+ *
+ * Author: Lasse Collin <lasse.collin@tukaani.org>
+ *
+ * This file has been put into the public domain.
+ * You can do whatever you want with this file.
+ */
+
+package org.tukaani.xz;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+class UncompressedLZMA2OutputStream extends FinishableOutputStream {
+    private final FinishableOutputStream out;
+    private final DataOutputStream outData;
+
+    private final byte[] uncompBuf
+            = new byte[LZMA2OutputStream.COMPRESSED_SIZE_MAX];
+    private int uncompPos = 0;
+    private boolean dictResetNeeded = true;
+
+    static int getMemoryUsage() {
+        // uncompBuf + a little extra
+        return 70;
+    }
+
+    UncompressedLZMA2OutputStream(FinishableOutputStream out) {
+        this.out = out;
+        outData = new DataOutputStream(out);
+    }
+
+    public void write(int b) throws IOException {
+        byte[] buf = new byte[1];
+        buf[0] = (byte)b;
+        write(buf, 0, 1);
+    }
+
+    public void write(byte[] buf, int off, int len) throws IOException {
+        if (off < 0 || len < 0 || off + len < 0 || off + len > buf.length)
+            throw new IllegalArgumentException();
+
+        while (len > 0) {
+            int copySize = Math.min(uncompBuf.length - uncompPos, len);
+            System.arraycopy(buf, off, uncompBuf, uncompPos, copySize);
+            len -= copySize;
+            uncompPos += copySize;
+
+            if (uncompPos == uncompBuf.length)
+                writeChunk();
+        }
+    }
+
+    private void writeChunk() throws IOException {
+        outData.writeByte(dictResetNeeded ? 0x01 : 0x02);
+        outData.writeShort(uncompPos - 1);
+        outData.write(uncompBuf, 0, uncompPos);
+        uncompPos = 0;
+    }
+
+    public void flush() throws IOException {
+        if (uncompPos > 0)
+            writeChunk();
+
+        out.flush();
+    }
+
+    public void finish() throws IOException {
+        if (uncompPos > 0)
+            writeChunk();
+
+        out.write(0x00);
+        out.finish();
+    }
+
+    public void close() throws IOException {
+        if (uncompPos > 0)
+            writeChunk();
+
+        out.write(0x00);
+        out.close();
+    }
+}

src/org/tukaani/xz/LZMA2Encoder.java
@@ -10,17 +10,26 @@
 package org.tukaani.xz;
 
 import java.io.OutputStream;
+import org.tukaani.xz.lzma.LZMAEncoder;
 
 class LZMA2Encoder extends LZMA2Coder implements FilterEncoder {
     private LZMA2Options options;
     private byte[] props = new byte[1];
 
     LZMA2Encoder(LZMA2Options options) {
+        if (options.getPresetDict() != null)
+            throw new IllegalArgumentException(
+                    "XZ doesn't support a preset dictionary for now");
+
+        if (options.getMode() == LZMA2Options.MODE_UNCOMPRESSED) {
+            props[0] = (byte)0;
+        } else {
+            int d = Math.max(options.getDictSize(), options.DICT_SIZE_MIN);
+            props[0] = (byte)(LZMAEncoder.getDistSlot(d - 1) - 23);
+        }
+
         // Make a private copy so that the caller is free to change its copy.
         this.options = (LZMA2Options)options.clone();
-
-        // TODO: Props!!!
-
     }
 
     public long getFilterID() {

src/org/tukaani/xz/LZMA2Options.java
@@ -11,39 +11,75 @@ package org.tukaani.xz;
 
 import java.io.InputStream;
 import java.io.IOException;
+import org.tukaani.xz.lz.LZEncoder;
+import org.tukaani.xz.lzma.LZMAEncoder;
 
 /**
- * Options for LZMA2.
+ * LZMA2 compression options.
  * <p>
- * FIXME: This is unfinished and things might change.
+ * While this allows setting the LZMA2 compression options in detail,
+ * often you only need <code>LZMA2Options()</code> or
+ * <code>LZMA2Options(int)</code>.
  */
 public class LZMA2Options extends FilterOptions {
     /**
-     * Default compression preset.
+     * Minimum valid compression preset level is 0.
+     */
+    public static final int PRESET_MIN = 0;
+
+    /**
+     * Maximum valid compression preset level is 9.
+     */
+    public static final int PRESET_MAX = 9;
+
+    /**
+     * Default compression preset level is 6.
      */
     public static final int PRESET_DEFAULT = 6;
 
     /**
-     * Minimum dictionary size.
+     * Minimum dictionary size is 4 KiB.
      */
     public static final int DICT_SIZE_MIN = 4096;
 
     /**
      * Maximum dictionary size for compression.
      * <p>
-     * FIXME? Decompression dictionary size can be bigger.
+     * FIXME? Decompressor supports bigger dictionaries. With HC4
+     * the encoder would too. With the current implementation of BT4
+     * we would hit the limits of signed ints in array indexing.
      */
-    public static final int DICT_SIZE_MAX = 128 << 20;
+    public static final int DICT_SIZE_MAX = 768 << 20;
 
     /**
-     * Maximum value for lc + lp.
+     * The default dictionary size is 8 MiB.
+     */
+    public static final int DICT_SIZE_DEFAULT = 8 << 20;
+
+    /**
+     * Maximum value for lc + lp is 4.
      */
     public static final int LC_LP_MAX = 4;
 
     /**
-     * Maximum value for pb.
+     * The default number of literal context bits is 3.
+     */
+    public static final int LC_DEFAULT = 3;
+
+    /**
+     * The default number of literal position bits is 0.
+     */
+    public static final int LP_DEFAULT = 0;
+
+    /**
+     * Maximum value for pb is 4.
      */
     public static final int PB_MAX = 4;
+
+    /**
+     * The default number of position bits is 2.
+     */
+    public static final int PB_DEFAULT = 2;
 
     /**
      * Compression mode: uncompressed.
@@ -55,71 +91,362 @@ public class LZMA2Options extends FilterOptions {
      * Compression mode: fast.
      * This is usually combined with a hash chain match finder.
      */
-    public static final int MODE_FAST = 1;
+    public static final int MODE_FAST = LZMAEncoder.MODE_FAST;
 
     /**
      * Compression mode: normal.
      * This is usually combined with a binary tree match finder.
      */
-    public static final int MODE_NORMAL = 2;
+    public static final int MODE_NORMAL = LZMAEncoder.MODE_NORMAL;
 
     /**
-     * Minimum value for <code>niceLen</code>.
+     * Minimum value for <code>niceLen</code> is 8.
      */
     public static final int NICE_LEN_MIN = 8;
 
     /**
-     * Maximum value for <code>niceLen</code>.
+     * Maximum value for <code>niceLen</code> is 273.
      */
     public static final int NICE_LEN_MAX = 273;
 
     /**
      * Match finder: Hash Chain 2-3-4
      */
-    public static final int MF_HC4 = 0x04;
+    public static final int MF_HC4 = LZEncoder.MF_HC4;
 
     /**
      * Match finder: Binary tree 2-3-4
      */
-    public static final int MF_BT4 = 0x14;
+    public static final int MF_BT4 = LZEncoder.MF_BT4;
+
+    private static final int[] presetToDictSize = {
+            1 << 18, 1 << 20, 1 << 21, 1 << 22, 1 << 22,
+            1 << 23, 1 << 23, 1 << 24, 1 << 25, 1 << 26 };
+
+    private static final int[] presetToDepthLimit = { 4, 8, 24, 48 };
 
     private int dictSize;
+    private byte[] presetDict = null;
+    private int lc;
+    private int lp;
+    private int pb;
+    private int mode;
+    private int niceLen;
+    private int mf;
+    private int depthLimit;
 
-/*
-    public int lc;
-    public int lp;
-    public int pb;
-    public int mode;
-    public int niceLen;
-    public int mf;
-    public int depth;
-*/
-
-    public LZMA2Options() {
+    /**
+     * Creates new LZMA2 options and sets them to the default values.
+     * This is equivalent to <code>LZMA2Options(PRESET_DEFAULT)</code>.
+     */
+    public LZMA2Options() throws UnsupportedOptionsException {
         setPreset(PRESET_DEFAULT);
     }
 
-    public LZMA2Options(int preset) {
+    /**
+     * Creates new LZMA2 options and sets them to the given preset.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>preset</code> is not supported
+     */
+    public LZMA2Options(int preset) throws UnsupportedOptionsException {
         setPreset(preset);
     }
 
-    public void setPreset(int preset) {
-        // TODO
-        dictSize = 8 << 20;
+    /**
+     * Creates new LZMA2 options and sets them to the given custom values.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          unsupported options were specified
+     */
+    public LZMA2Options(int dictSize, int lc, int lp, int pb, int mode,
+                        int niceLen, int mf, int depth)
+            throws UnsupportedOptionsException {
+        setDictSize(dictSize);
+        setLcLp(lc, lp);
+        setPb(pb);
+        setMode(mode);
+        setNiceLen(niceLen);
+        setMatchFinder(mf);
+        setDepthLimit(depthLimit);
     }
 
+    /**
+     * Sets the compression options to the given preset.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>preset</code> is not supported
+     */
+    public void setPreset(int preset) throws UnsupportedOptionsException {
+        if (preset < 0 || preset > 9)
+            throw new UnsupportedOptionsException(
+                    "Unsupported preset: " + preset);
+
+        lc = LC_DEFAULT;
+        lp = LP_DEFAULT;
+        pb = PB_DEFAULT;
+        dictSize = presetToDictSize[preset];
+
+        if (preset <= 3) {
+            mode = MODE_FAST;
+            mf = MF_HC4;
+            niceLen = preset <= 1 ? 128 : NICE_LEN_MAX;
+            depthLimit = presetToDepthLimit[preset];
+        } else {
+            mode = MODE_NORMAL;
+            mf = MF_BT4;
+            niceLen = (preset == 4) ? 16 : (preset == 5) ? 32 : 64;
+            depthLimit = 0;
+        }
+    }
+
+    /**
+     * Sets the dictionary size in bytes.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>dictSize</code> is not supported
+     */
+    public void setDictSize(int dictSize) throws UnsupportedOptionsException {
+        if (dictSize < DICT_SIZE_MIN)
+            throw new UnsupportedOptionsException(
+                    "LZMA2 dictionary size must be at least 4 KiB: "
+                    + dictSize + " B");
+
+        if (dictSize > DICT_SIZE_MAX)
+            throw new UnsupportedOptionsException(
+                    "LZMA2 dictionary size must not exceed "
+                    + (DICT_SIZE_MAX >> 20) + " MiB: " + dictSize + " B");
+
+        this.dictSize = dictSize;
+    }
+
+    /**
+     * Gets the dictionary size in bytes.
+     */
+    public int getDictSize() {
+        return dictSize;
+    }
+
+    /**
+     * Sets the preset dictionary. Use null to disable the use of
+     * a preset dictionary. By default there is no preset dictionary.
+     * <p>
+     * <b>The .xz format doesn't support a preset dictionary for now.
+     * Do not set a preset dictionary unless you use raw LZMA2.</b>
+     */
+    public void setPresetDict(byte[] presetDict) {
+        this.presetDict = presetDict;
+    }
+
+    /**
+     * Gets the preset dictionary.
+     */
+    public byte[] getPresetDict() {
+        return presetDict;
+    }
+
+    /**
+     * Sets the number of literal context bits and literal position bits.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>lc</code> and <code>lp</code>
+     *                          are invalid
+     */
+    public void setLcLp(int lc, int lp) throws UnsupportedOptionsException {
+        if (lc < 0 || lp < 0 || lc > LC_LP_MAX || lp > LC_LP_MAX
+                || lc + lp > LC_LP_MAX)
+            throw new UnsupportedOptionsException(
+                    "lc + lp must not exceed " + LC_LP_MAX + ": "
+                    + lc + " + " + lp);
+
+        this.lc = lc;
+        this.lp = lp;
+    }
+
+    /**
+     * Sets the number of literal context bits.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>lc</code> is invalid, or the sum
+     *                          of <code>lc</code> and <code>lp</code>
+     *                          exceed LC_LP_MAX
+     */
+    public void setLc(int lc) throws UnsupportedOptionsException {
+        setLcLp(lc, lp);
+    }
+
+    /**
+     * Sets the number of literal position bits.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>lp</code> is invalid, or the sum
+     *                          of <code>lc</code> and <code>lp</code>
+     *                          exceed LC_LP_MAX
+     */
+    public void setLp(int lp) throws UnsupportedOptionsException {
+        setLcLp(lc, lp);
+    }
+
+    /**
+     * Gets the number of literal context bits.
+     */
+    public int getLc() {
+        return lc;
+    }
+
+    /**
+     * Gets the number of literal position bits.
+     */
+    public int getLp() {
+        return lp;
+    }
+
+    /**
+     * Sets the number of position bits.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>pb</code> is invalid
+     */
+    public void setPb(int pb) throws UnsupportedOptionsException {
+        if (pb < 0 || pb > PB_MAX)
+            throw new UnsupportedOptionsException(
+                    "pb must not exceed " + PB_MAX + ": " + pb);
+
+        this.pb = pb;
+    }
+
+    /**
+     * Gets the number of position bits.
+     */
+    public int getPb() {
+        return pb;
+    }
+
+    /**
+     * Sets the compression mode.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>mode</code> is not supported
+     */
+    public void setMode(int mode) throws UnsupportedOptionsException {
+        if (mode != MODE_FAST && mode != MODE_NORMAL)
+            throw new UnsupportedOptionsException(
+                    "Unsupported compression mode: " + mode);
+
+        this.mode = mode;
+    }
+
+    /**
+     * Gets the compression mode.
+     */
+    public int getMode() {
+        return mode;
+    }
+
+    /**
+     * Sets the nice length of matches.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>nice</code> is invalid
+     */
+    public void setNiceLen(int niceLen) throws UnsupportedOptionsException {
+        if (niceLen < NICE_LEN_MIN)
+            throw new UnsupportedOptionsException(
+                    "Minimum nice length of matches is "
+                    + NICE_LEN_MIN + " bytes: " + niceLen);
+
+        if (niceLen > NICE_LEN_MAX)
+            throw new UnsupportedOptionsException(
+                    "Maximum nice length of matches is " + NICE_LEN_MAX
+                    + ": " + niceLen);
+
+        this.niceLen = niceLen;
+    }
+
+    /**
+     * Gets the nice length of matches.
+     */
+    public int getNiceLen() {
+        return niceLen;
+    }
+
+    /**
+     * Sets the match finder type.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>mf</code> is not supported
+     */
+    public void setMatchFinder(int mf) throws UnsupportedOptionsException {
+        if (mf != MF_HC4 && mf != MF_BT4)
+            throw new UnsupportedOptionsException(
+                    "Unsupported match finder: " + mf);
+
+        this.mf = mf;
+    }
+
+    /**
+     * Gets the match finder type.
+     */
+    public int getMatchFinder() {
+        return mf;
+    }
+
+    /**
+     * Sets the match finder search depth limit.
+     * <p>
+     * The special value of <code>0</code> indicates that the depth limit
+     * should be automatically calculated by the selected match finder
+     * from the nice length of matches.
+     *
+     * @throws      UnsupportedOptionsException
+     *                          <code>depthLimit</code> is invalid
+     */
+    public void setDepthLimit(int depthLimit)
+            throws UnsupportedOptionsException {
+        if (depthLimit < 0)
+            throw new UnsupportedOptionsException(
+                    "Depth limit cannot be negative: " + depthLimit);
+
+        this.depthLimit = depthLimit;
+    }
+
+    /**
+     * Gets the match finder search depth limit.
+     */
+    public int getDepthLimit() {
+        return depthLimit;
+    }
+
+    /**
+     * Gets how much memory the encoder will need with these options.
+     */
     public int getEncoderMemoryUsage() {
-        return LZMA2OutputStream.getMemoryUsage(this);
+        return (mode == MODE_UNCOMPRESSED)
+               ? UncompressedLZMA2OutputStream.getMemoryUsage()
+               : LZMA2OutputStream.getMemoryUsage(this);
     }
 
+    /**
+     * Gets a raw LZMA2 encoder output stream using these options.
+     */
     public FinishableOutputStream getOutputStream(FinishableOutputStream out) {
+        if (mode == MODE_UNCOMPRESSED)
+            return new UncompressedLZMA2OutputStream(out);
+
         return new LZMA2OutputStream(out, this);
     }
 
+    /**
+     * Gets how much memory the decoder will need to decompress data
+     * that was encoded with these options.
+     */
     public int getDecoderMemoryUsage() {
         return LZMA2InputStream.getMemoryUsage(dictSize);
     }
 
+    /**
+     * Gets a raw LZMA2 decoder output stream using these options.
+     */
     public InputStream getInputStream(InputStream in) throws IOException {
         return new LZMA2InputStream(in, dictSize);
     }
@@ -132,7 +459,7 @@ public class LZMA2Options extends FilterOptions {
         try {
             return super.clone();
         } catch (CloneNotSupportedException e) {
-            // Never reached
+            assert false;
             throw new RuntimeException();
         }
     }

src/org/tukaani/xz/LZMA2OutputStream.java
@@ -10,23 +10,60 @@
 
 package org.tukaani.xz;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
-
-//
-// TODO: This creates a valid LZMA2 stream but it doesn't compress.
-//       So this is useless except for testing the .xz container support.
-//
+import org.tukaani.xz.lz.LZEncoder;
+import org.tukaani.xz.rangecoder.RangeEncoder;
+import org.tukaani.xz.lzma.LZMAEncoder;
 
 class LZMA2OutputStream extends FinishableOutputStream {
+    static final int COMPRESSED_SIZE_MAX = 64 << 10;
+
     private final FinishableOutputStream out;
+    private final DataOutputStream outData;
+
+    private final LZEncoder lz;
+    private final RangeEncoder rc;
+    private final LZMAEncoder lzma;
+
+    private int props;
+    private boolean dictResetNeeded = true;
+    private boolean stateResetNeeded = true;
+    private boolean propsNeeded = true;
+
+    private int pendingSize = 0;
+
+    private static int getExtraSizeBefore(int dictSize) {
+        return COMPRESSED_SIZE_MAX > dictSize
+               ? COMPRESSED_SIZE_MAX - dictSize : 0;
+    }
 
     static int getMemoryUsage(LZMA2Options options) {
-        // TODO
-        return 1;
+        // 64 KiB buffer for the range encoder + a little extra + LZMAEncoder
+        int dictSize = options.getDictSize();
+        int extraSizeBefore = getExtraSizeBefore(dictSize);
+        return 70 + LZMAEncoder.getMemoryUsage(options.getMode(),
+                                               dictSize, extraSizeBefore,
+                                               options.getMatchFinder());
     }
 
     LZMA2OutputStream(FinishableOutputStream out, LZMA2Options options) {
         this.out = out;
+        outData = new DataOutputStream(out);
+        rc = new RangeEncoder(COMPRESSED_SIZE_MAX);
+
+        int dictSize = options.getDictSize();
+        int extraSizeBefore = getExtraSizeBefore(dictSize);
+        lzma = LZMAEncoder.getInstance(rc,
+                options.getLc(), options.getLp(), options.getPb(),
+                options.getMode(),
+                dictSize, extraSizeBefore, options.getNiceLen(),
+                options.getMatchFinder(), options.getDepthLimit());
+
+        lz = lzma.getLZEncoder();
+        lz.setPresetDict(dictSize, options.getPresetDict());
+
+        props = (options.getPb() * 5 + options.getLp()) * 9 + options.getLc();
     }
 
     public void write(int b) throws IOException {
@@ -39,30 +76,95 @@ class LZMA2OutputStream extends FinishableOutputStream {
         if (off < 0 || len < 0 || off + len < 0 || off + len > buf.length)
             throw new IllegalArgumentException();
 
-        while (off > 0x10000) {
-            writeChunk(buf, off, 0x10000);
-            off += 0x10000;
-            len -= 0x10000;
-        }
+        while (len > 0) {
+            int used = lz.fillWindow(buf, off, len);
+            off += used;
+            len -= used;
+            pendingSize += used;
 
-        writeChunk(buf, off, len);
+            if (lzma.encodeForLZMA2())
+                writeChunk();
+        }
     }
 
-    private void writeChunk(byte[] buf, int off, int len) throws IOException {
-        out.write(0x01);
-        out.write((len - 1) >>> 8);
-        out.write(len - 1);
-        out.write(buf, off, len);
+    private void writeChunk() throws IOException {
+        int compressedSize = rc.finish();
+        int uncompressedSize = lzma.getUncompressedSize();
+
+        // +2 because the header of a compressed chunk is 2 bytes
+        // bigger than the header of an uncompressed chunk.
+        if (compressedSize + 2 < uncompressedSize)
+            writeLZMA(uncompressedSize, compressedSize);
+        else
+            writeUncompressed(uncompressedSize);
+
+        pendingSize -= uncompressedSize;
+        lzma.resetUncompressedSize();
+        rc.reset();
+    }
+
+    private void writeLZMA(int uncompressedSize, int compressedSize)
+            throws IOException {
+        int control;
+
+        if (propsNeeded) {
+            if (dictResetNeeded)
+                control = 0x80 + (3 << 5);
+            else
+                control = 0x80 + (2 << 5);
+        } else {
+            if (stateResetNeeded)
+                control = 0x80 + (1 << 5);
+            else
+                control = 0x80;
+        }
+
+        control |= (uncompressedSize - 1) >>> 16;
+        outData.writeByte(control);
+
+        outData.writeShort(uncompressedSize - 1);
+        outData.writeShort(compressedSize - 1);
+
+        if (propsNeeded)
+            outData.writeByte(props);
+
+        rc.write(out);
+
+        propsNeeded = false;
+        stateResetNeeded = false;
+        dictResetNeeded = false;
+    }
+
+    private void writeUncompressed(int uncompressedSize) throws IOException {
+        outData.writeByte(dictResetNeeded ? 0x01 : 0x02);
+        outData.writeShort(uncompressedSize - 1);
+        lzma.copyUncompressed(out);
+
+        lzma.reset();
+        stateResetNeeded = true;
+        dictResetNeeded = false;
     }
 
     private void writeEndMarker() throws IOException {
-        // TODO: Flush incomplete chunk.
+        lz.setFinishing();
+
+        while (pendingSize > 0) {
+            lzma.encodeForLZMA2();
+            writeChunk();
+        }
+
         out.write(0x00);
     }
 
     public void flush() throws IOException {
-        throw new UnsupportedOptionsException(
-                "Flushing LZMA2OutputStream not implemented yet");
+        lz.setFlushing();
+
+        while (pendingSize > 0) {
+            lzma.encodeForLZMA2();
+            writeChunk();
+        }
+
+        out.flush();
     }
 
     public void finish() throws IOException {

src/org/tukaani/xz/XZOutputStream.java
@@ -183,7 +183,7 @@ public class XZOutputStream extends FinishableOutputStream {
      * Equivalent of liblzma's LZMA_SYNC_FLUSH might be implemented in
      * the future, and perhaps should be what <code>flush()</code> should do.
      */
-    public void flush() throws IOException {
+    public void flushBlock() throws IOException {
         if (exception != null)
             throw exception;
 
@@ -200,6 +200,13 @@ public class XZOutputStream extends FinishableOutputStream {
         }
 
         out.flush();
+    }
+
+    public void flush() throws IOException {
+        if (blockEncoder != null)
+            blockEncoder.flush();
+        else
+            out.flush();
     }
 
     /**
@@ -219,7 +226,7 @@ public class XZOutputStream extends FinishableOutputStream {
         if (!finished) {
             // flush() checks for pending exceptions so we don't need to
             // worry about it here.
-            flush();
+            flushBlock();
 
             try {
                 index.encode(out);

commit a16978457f7e1e0fabd70e44f86f83fb5f44f7a4

Fix data corruption with LZMA2Options.MODE_UNCOMPRESSED
It can happen when encoding a file larger than 65536 bytes. Decoders
catch the corruption because the integrity check doesn't match.

The encoder bug doesn't occur if one only adds

    options.setMode(LZMA2Options.MODE_UNCOMPRESSED);

to XZEncDemo.java because XZEncDemo uses 8192-byte input buffer
which perfectly divides 65536. If the buffer size is changed, then
XZEncDemo will trigger the bug.

Reported-by: Daniel Wilhelm
Fixes: daface1a5537 ("Add LZMA2 encoder.")
Fixes: https://github.com/tukaani-project/xz-java/issues/21

src/org/tukaani/xz/UncompressedLZMA2OutputStream.java
@@ -66,6 +66,7 @@ class UncompressedLZMA2OutputStream extends FinishableOutputStream {
                                         - uncompPos, len);
                 System.arraycopy(buf, off, uncompBuf, uncompPos, copySize);
                 len -= copySize;
+                off += copySize;
                 uncompPos += copySize;
 
                 if (uncompPos == LZMA2OutputStream.COMPRESSED_SIZE_MAX)