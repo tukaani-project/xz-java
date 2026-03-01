tukaani-project/xz-java

commit 9fd7bc0f1e0e48b09df7b1c06cd2dd1658458c99

Add MatchLength.EXTRA_SIZE and use it in LZEncoder
It's not needed with the basic MatchLength version but
an optimized version will need it.

src/org/tukaani/xz/lz/MatchLength.java
@@ -5,6 +5,8 @@
 package org.tukaani.xz.lz;
 
 final class MatchLength {
+    static final int EXTRA_SIZE = 0;
+
     static int getLen(byte[] buf, int off, int delta, int len, int lenLimit) {
         assert off >= 0;
         assert delta > 0;

src/org/tukaani/xz/lz/LZEncoder.java
@@ -136,7 +136,10 @@ public abstract class LZEncoder {
               int niceLen, int matchLenMax, ArrayCache arrayCache) {
         bufSize = getBufSize(dictSize, extraSizeBefore, extraSizeAfter,
                              matchLenMax);
-        buf = arrayCache.getByteArray(bufSize, false);
+
+        // MatchLength.getLen might read and ignore extra bytes
+        // at the end of the buffer.
+        buf = arrayCache.getByteArray(bufSize + MatchLength.EXTRA_SIZE, false);
 
         keepSizeBefore = extraSizeBefore + dictSize;
         keepSizeAfter = extraSizeAfter + matchLenMax;

commit ac1aeb197dd8589dc6892f13f5ef68801383ec34

Fix ArrayIndexOutOfBoundsException in the LZMA/LZMA2 encoder
The extra size constant from UnalignedLongLEMatchLengthFinder didn't take
effect as intended. This could result in ArrayIndexOutOfBoundsException.
Frequent calls to flush() increased the likelyhood of hitting the bug
but it could be triggered without flushing too:

    ant clean jar
    head -c12853521 /dev/zero \
        | java -ea -jar build/jar/XZEncDemo.jar > /dev/null

The bug is present in versions 1.10 and 1.11. If one cannot upgrade,
one should set the property org.tukaani.xz.MatchLengthFinder=Basic
which disables UnalignedLongLEMatchLengthFinder. Example:

    head -c12853521 /dev/zero \
        | java -Dorg.tukaani.xz.MatchLengthFinder=Basic \
               -jar build/jar/XZEncDemo.jar > /dev/null

Fixes: 9fd7bc0f1e0e ("Add MatchLength.EXTRA_SIZE and use it in LZEncoder")
Fixes: https://github.com/tukaani-project/xz-java/issues/24

src/org/tukaani/xz/lz/MatchLength.java
@@ -6,7 +6,14 @@ package org.tukaani.xz.lz;
 
 // See the version in the src9 directory for documentation.
 final class MatchLength {
-    static final int EXTRA_SIZE = 0;
+    // Initialize this in "static {}" below. If this was initialized here
+    // instead, compiler would pick the 0 and never see non-zero value when
+    // this class is replaced by the version in the src9 directory.
+    static final int EXTRA_SIZE;
+
+    static {
+        EXTRA_SIZE = 0;
+    }
 
     static int getLen(byte[] buf, int off, int delta, int len, int lenLimit) {
         assert off >= 0;